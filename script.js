fdbffc1617f2bc9c71139aff0415894841a9140d
lk140 <max.hemp@icloud.com>
2025-12-29 18:27:57 +0000

    oijkerbhgiure

---
+++ script.js
@@ -0,0 +1,14 @@
+const titre3 = document.querySelector('#b1')
+if (titre3) {
+  titre3.addEventListener('click', () => {
+    window.location.href = "index.html";
+  });
+}
+
+
+const titre4 = document.querySelector('#b2')
+if (titre4) {
+  titre4.addEventListener('click', () => {
+    window.location.href = "info.html";
+  });
+}
